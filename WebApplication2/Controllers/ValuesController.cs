2236c47c1a0ca2bb29a8614409afc81882641b6a
teamzz111 <andresf.largo@gmail.com>
2019-01-14 16:20:09 -0500

    Initial Commit

---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class dbConnect
+    {
+        private string databaseName = string.Empty;
+        private MySqlConnection connection;
+        public string DatabaseName
+        {
+            get{ return databaseName; }
+            set { this.databaseName = value; }
+        }
+
+        public string Password { get; set; }
+    }
+}
